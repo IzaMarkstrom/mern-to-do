569a330f6f313c33388aa9776d91b4970b12b820
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-04-01 10:07:04 +0200

    Added JWT and login route

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,6 +1,6 @@
 // Declaring what dependency I want.
 const express = require("express");
-const app = express()
+const jwt = require("jsonwebtoken")
 const router = express.Router()
 const bodyParser = require("body-parser")
 const { User } = require("../models/UserSchema")
@@ -9,56 +9,43 @@ const bcrypt = require("bcryptjs")
 // Created a new user.
 router.post("/user/create", async (req, res) => {
     const {username, password} = req.body    
+
+    const userExists = await User.findOne({ username });
+    
+    if(userExists){
+        res.status(403).json({ message: "Username already exists" })
+    }
+
     const user = new User ({username, password})
     await user.save()
     res.json(username)
 });
 
+router.post("/login", async (req, res) => {
+    const {username, password} = req.body 
+
+    const userExists = await User.findOne({ username })
+
+    if (userExists) {
+        const validPassword = await bcrypt.compare(password, userExists.password)
+
+        if(validPassword){
+            jwt.sign({ username, id: userExists._id.toString() }, process.env.JWT_SECRET, (err, token) => {
+
+                if (err) {
+                    res.status(401).json(err)
+                } else {
+                    res.status(200).json({ token })
+                }
+            })
+        } else {
+            res.status(403).json({ message: "Wrong password" })
+        }
+    } else {
+        res.status(403).json({ message: "Username does not exist" })
+    }
+
+})
 
 
-
-// // Handles the registration of the new user.
-// router.post("/user/create", async (req, res, next) => {
-//     const username = req.body.username.trim()
-//     const password = req.body.password
-
-//     const payload = req.body
-
-//     if(username && password){
-//         // This will go into the database and check for user.
-//         const user = await User.findOne({ username: username})
-//         .catch((error) => {
-//             console.log(error)
-//             payload.errorMessage = "Something went wrong."
-//             res.status(200).render("register", payload)
-//         })
- 
-//         // No user found.
-//         // If we didn't find a user, a new user will be created.
-//         if(user == null){
-//             const data = req.body
-//             // We salt the password 2 raised to 10 times.
-//             data.password = await bcrypt.hash(password, 10)
-
-//             User.create(data)
-//             .then((user) => {
-//                 // We store newly created user in the session of the user property.
-//                 req.session.user = user
-//                 return res.redirect("/")
-//             })
-
-//         // User found.
-//         // If we did find a user, we output error message.
-//         } else {
-//             payload.errorMessage = "Username already exists."
-//             res.status(200).render("register", payload)
-//         }
-//     } else {
-//         payload.errorMessage = "Make sure each field is filled in."
-//         res.status(200).render("register", payload)
-//     }
-    
-// })
-
-// Export it so we can use this file in other places.
 module.exports = router
