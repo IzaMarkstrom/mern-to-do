7096a6995893c12e587a641fca45fd1e2d2614e1
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-04-04 10:58:08 +0200

    Added html for todos

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+      <h1>Hello ${} Iza</h1>
+      <h4>Your tasks</h4>
+      
+      <div className="todos">
+        <div className="todo">
+          <div className="checkbox"></div>
+
+          <div className="text">Buy chocolate</div>
+
+          <div className="delete-todo">x</div>
+        </div>
+        <div className="todo is-complete">
+          <div className="checkbox"></div>
+
+          <div className="text">Buy milk</div>
+
+          <div className="delete-todo">x</div>
+        </div>
+      </div>
+    </div>
   )
 }
