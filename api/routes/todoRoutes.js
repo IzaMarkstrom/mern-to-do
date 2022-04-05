9c6b1d6bac6b275189d3e3072ab0d485bbadf17f
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-04-05 14:32:03 +0200

    Added todoRoutes

--- api/routes/todoRoutes.js
+++ api/routes/todoRoutes.js
@@ -2,9 +2,12 @@
 const express = require("express");
 const router = express.Router()
 const { authUser } = require("../controllers/auth")
+const todos = require("../controllers/todos")
 
 // User routes
-router.post("/homepage", authUser);
+router.get("/homepage", authUser, todos.listAllTodos);
+router.post("/homepage", authUser, todos.createTodos);
+router.get("/homepage", authUser, todos.getDoneTodos);
 
 
 module.exports = router
