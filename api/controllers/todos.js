572c0d1300465078cb571dac62e304a0df9c5c1e
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-04-10 20:02:56 +0200

    Added controllers for todos

---
+++ api/controllers/todos.js
@@ -0,0 +1,35 @@
+const { createNewTodo, getAllTodos, setCompleteStatus, Todo } = require("../models/TodoSchema")
+
+const listAllTodos = async (req, res) => {
+    const todoData = await getAllTodos()
+
+    if(todoData !== null){
+        res.status(200).json({ todoData })
+    } else {
+        res.status(404).json({ message: "No list found" })
+    }
+    
+}
+
+const createTodos = async (req, res) => {
+    const todoData = req.body.text
+
+    const todo = await createNewTodo(todoData)
+    res.status(200).json({ todo })
+}
+
+const completeTodos = async (req, res) => {
+    const todoId = req.params.id
+    const todo = await setCompleteStatus(todoId)
+
+    res.status(200).json({ todo })
+}
+
+const deleteTodos = async (req, res) => {
+    const todoId = req.params.id
+    const result = await Todo.findByIdAndDelete(todoId)
+
+    res.json(result)
+}
+
+module.exports = { listAllTodos, createTodos, completeTodos, deleteTodos }
