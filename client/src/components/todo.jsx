b645428eedda2b695b332c69c870163384d0182c
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-04-10 20:03:36 +0200

    Rendring todos on website

--- client/src/components/todo.jsx
+++ client/src/components/todo.jsx
@@ -1,22 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { BASE_API } from "../utils"
 
 export const RenderTodos = () => {
+    const [todos, setTodos] = useState([])
+    const [newTodo, setNewTodo] = useState("")
+  
+    useEffect(() => {
+      fetch(`${BASE_API}/todos/homepage`)
+        .then(res => res.json())
+        .then(data => setTodos(data.todoData))
+        .then(sortTodosByTime(todos))
+        .catch(err => console.error(`Error: ${err}`))
+    }, [])
+
+    function setTime(createdAt){
+        return createdAt.substring(0, 16).replace("T", " kl ")
+    }
+    
+    function sortTodosByTime(todos1) {
+        const reversedTodos = todos1.reverse()
+        setTodos(reversedTodos)
+        console.log(todos)
+    }
+
+    const completeTodo = async id => {
+        const data = await fetch(`${BASE_API}/todo/complete/${id}`)
+            .then(res => res.json())
+
+            setTodos(todos => todos.map(todo => {
+                if(todo._id === data._id) {
+                    todo.complete = data.complete
+                }
+                return todo
+            }))
+    }
+
+
     return (
         <>
-        <div className="todo">
-            <div className="checkbox"></div>
-
-            <div className="text">Buy chocolate</div>
-
-            <div className="delete-todo">x</div>
-        </div>
-        <div className="todo is-complete">
-            <div className="checkbox"></div>
-
-            <div className="text">Buy milk</div>
-
-            <div className="delete-todo">x</div>
-        </div>
+        {todos.map(todo => (
+            <div className={"todo " + (todo.complete ? "is-complete" : "")}
+            key={todo._id} onClick={() => completeTodo(todo._id)}>
+                <div className="checkbox"></div>
+        
+                <div className="text">{todo.text}</div>
+        
+                <div className="delete-todo">x</div>
+                <div className="timestamp">{setTime(todo.createdAt)}</div>
+            </div>
+        ))}
         </>
     )
 }
