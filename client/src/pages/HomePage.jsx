6453ec649012f92985952fa75a7d1c173df2a640
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-04-10 20:04:06 +0200

    Removed test code

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,26 +1,15 @@
 import React from 'react'
+import { RenderTodos } from '../components/todo'
 
 export default function HomePage() {
+
   return (
     <div>
       <h1>Hello ${} Iza</h1>
-      <h4>Your tasks</h4>
+      <h3>Your tasks</h3>
       
       <div className="todos">
-        <div className="todo">
-          <div className="checkbox"></div>
-
-          <div className="text">Buy chocolate</div>
-
-          <div className="delete-todo">x</div>
-        </div>
-        <div className="todo is-complete">
-          <div className="checkbox"></div>
-
-          <div className="text">Buy milk</div>
-
-          <div className="delete-todo">x</div>
-        </div>
+        <RenderTodos/>
       </div>
     </div>
   )
