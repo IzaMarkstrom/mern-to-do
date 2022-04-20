05fba8a13e219c18680adad4e3df007c5b8de691
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-04-20 18:13:48 +0200

    Added useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,38 @@ import { Routes, Route } from "react-router-dom"
 import RegisterPage from "./pages/RegisterPage"
 import LoginPage from "./pages/LoginPage"
 import HomePage from "./pages/HomePage";
+import { getTodoList } from "./components/API";
 
 const Context = createContext()
 
 function App() {
   const [username, setUsername] = useState("")
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(null)
 
-  // useEffect(() => {
-  //   getUser()
-  //   getTodos()
-  // })
+  useEffect(async () => {
+    getTodos()
+    // getUser()
+  })
 
-  // const getUser = async () => {
+  const getTodos = async () => {
+    await getTodoList()
+    .then(res => res.json())
+    .then(data => setTodos(data.todoData))
+    .catch(err => console.error(`Error: ${err}`))
+}
 
-  // }
+
+// const getUser = async () => {
+//   await getTodoList()
+//   .then(res => res.json())
+//   .then(data => setTodos(data.todoData))
+//   .catch(err => console.error(`Error: ${err}`))
+// }
+
 
   return (
     <Context.Provider value={{ username, setUsername, todos, setTodos}}>
-      <div className="bodyContainer">
+      <div className="">
         <Routes>
           <Route path="/register" element={<RegisterPage/>} />
           <Route path="/" element={<LoginPage/>} />
