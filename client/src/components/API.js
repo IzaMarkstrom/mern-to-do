bf186d7bd326b0d622fab1451a5fe9183a7fb2b4
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-04-22 20:48:58 +0200

    Added Bearer token

--- client/src/components/API.js
+++ client/src/components/API.js
@@ -17,32 +17,44 @@ async function loginUser(payload) {
 }
 
 async function getTodoList() {
+    const token = localStorage.getItem("todoList")
+
     return fetch(`${BASE_API}/todos/homepage`, {
         method: "GET",
-        headers: { "Content-Type": "application/json" },
+        headers: { "Content-Type": "application/json", 
+                    "Authorization": `Bearer ${token}` },
         body: JSON.stringify()
     })
 }
 
 async function getCompletedTodoList() {
+    const token = localStorage.getItem("todoList")
+
     return fetch(`${BASE_API}/todos/homepage/completedTodos`, {
         method: "GET",
-        headers: { "Content-Type": "application/json" },
+        headers: { "Content-Type": "application/json", 
+        "Authorization": `Bearer ${token}` },
         body: JSON.stringify()
     })
 }
 
 async function completeTodo(todoId) {
+    const token = localStorage.getItem("todoList")
+
     return fetch(`${BASE_API}/todos/${todoId}`, {
         method: "PUT",
-        headers: { "Content-Type": "application/json" }
+        headers: { "Content-Type": "application/json", 
+        "Authorization": `Bearer ${token}` },
     })
 }
 
 async function createNewTodo(payload) {
+    const token = localStorage.getItem("todoList")
+
     return fetch(`${BASE_API}/todos/homepage`, {
         method: "POST",
-        headers: { "Content-Type": "application/json"},
+        headers: { "Content-Type": "application/json", 
+        "Authorization": `Bearer ${token}` },
         body: JSON.stringify({text: payload})
     })
 }
