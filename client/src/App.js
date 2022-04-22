6f7ea58d52f44a10c81d4ac01249d9f48643fdd6
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-04-22 22:19:40 +0200

    Added reload useState

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,19 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState } from "react";
 import { Routes, Route } from "react-router-dom"
 import RegisterPage from "./pages/RegisterPage"
 import LoginPage from "./pages/LoginPage"
 import HomePage from "./pages/HomePage";
-import { getTodoList } from "./components/API";
 
 const Context = createContext()
 
 function App() {
   const [username, setUsername] = useState("")
   const [todos, setTodos] = useState(null)
-
-  useEffect(() => {
-    getTodos()
-    // getUser()
-  }, [])
-
-  const getTodos = async () => {
-    await getTodoList()
-    .then(res => res.json())
-    .then(data => setTodos(data.todoData))
-    .catch(err => console.error(`Error: ${err}`))
-    
-}
-
-
-// const getUser = async () => {
-//   await getTodoList()
-//   .then(res => res.json())
-//   .then(data => setTodos(data.todoData))
-//   .catch(err => console.error(`Error: ${err}`))
-// }
+  const [reload, setReload] = useState(false)
 
 
   return (
-    <Context.Provider value={{ username, setUsername, todos, setTodos }}>
+    <Context.Provider value={{ username, setUsername, todos, setTodos, reload, setReload }}>
       <div className="">
         <Routes>
           <Route path="/register" element={<RegisterPage/>} />
