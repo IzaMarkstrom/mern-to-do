10acc243cbba5c23006b9d06a77a8768a1330b06
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-04-22 09:45:20 +0200

    Changed the link to correct href

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -49,7 +49,7 @@ export default function RegisterPage() {
                 />
                 <input type="submit" value="Submit" className='loginBtn' />
             </form>
-          <p className="mt-2">Already a user? Click <a href="/login" className="loginLink">here</a> to login</p>
+          <p className="mt-2">Already a user? Click <a href="/" className="loginLink">here</a> to login</p>
       </div>
   </div>
   )
