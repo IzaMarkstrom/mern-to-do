445c0162f278f2952a9404d94888b313bfedd262
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-04-22 22:20:23 +0200

    Added useEffect to render all todos from start

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect, useContext} from 'react'
 import CreateTodo from '../components/CreateTodo'
 import GetAllTodos from '../components/GetAllTodos'
 import GetCompletedTodos from '../components/GetCompletedTodos'
 import Logout from '../components/Logout'
 import TodoList from '../components/TodoList'
+import { Context } from '../App'
+import { getTodoList } from '../components/API'
 
 
 export default function HomePage() {
+  const { setTodos, reload, setReload } = useContext(Context)
 
+  useEffect(() => {
+    getTodos()
+    // getUser()
+  }, [reload])
+
+  const getTodos = async () => {
+    await getTodoList()
+    .then(res => res.json())
+    .then(data => setTodos(data.todoData))
+    .then(setReload(true))
+    .catch(err => console.error(`Error: ${err}`))
+    
+}
   return (
     <div>
       <Logout/>
