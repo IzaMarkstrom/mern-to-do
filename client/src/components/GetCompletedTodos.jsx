9700a61bece8d1e535ad4f4831d070d76131cf92
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-04-22 20:49:41 +0200

    Button to get all complete todos

--- client/src/components/GetCompletedTodos.jsx
+++ client/src/components/GetCompletedTodos.jsx
@@ -3,20 +3,15 @@ import { Context } from "../App"
 import { getCompletedTodoList } from "./API"
 
 export default function GetCompletedTodos() {
-  const { todos, setTodos } = useContext(Context)
+  const { setTodos } = useContext(Context)
 
     const handleOnSubmit = async (e) => {
       e.preventDefault()
 
         await getCompletedTodoList()
         .then(res => res.json())
-        .then(data => console.log(data.todoData))
-        // console.log(todos)
+        .then(data => setTodos(data.todoData))
     }
-    
-  //   useEffect(()=>{
-  //     console.log("todos updated")
-  //  },[todos])
 
   return (
     <button className='completedTodosButton' onClick={handleOnSubmit}>Completed todos</button>
