058f85f13b7f4f06e31e1ed33a87b11d41c641cf
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-04-24 14:03:44 +0200

    Added create tags file

---
+++ client/src/components/CreateTags.jsx
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react'
+import { Context } from '../App'
+
+export default function CreateTags() {
+    const { tags, setTags } = useContext(Context)
+
+    function handleKeyDown(e){
+        if(e.key !== "Enter") return
+
+        const value = e.target.value
+
+        if(!value.trim()) return
+
+        setTags([...tags, value])
+        e.target.value = ""
+    }
+
+    function removeTag(index){
+        setTags(tags.filter((el, i) => i !== index))
+    }
+
+  return (
+    <div className='tagsContainer'>
+        <input onKeyDown={handleKeyDown} type="text" className='newTagInput' placeholder='  Add tags'/>
+        {tags.map((tag, index) => (
+            <div className='tagItem' key={index}>
+                <span className='tagSpan'>{tag}</span>
+                <span className='removeTagButton' onClick={() => removeTag(index)}>x</span>
+            </div>
+            )
+        )}
+    </div>
+  )
+}
