98e109056e73a39eaceade65408766ee726e6424
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-04-26 13:41:49 +0200

    Imported getUser from user controllers

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,11 +1,13 @@
 // Declaring what dependency I want.
 const express = require("express");
 const router = express.Router()
-const { createUser, loginUser } = require("../controllers/users")
+const { createUser, loginUser, getUser } = require("../controllers/users")
+const { authUser } = require("../controllers/auth")
 
 // User routes
 router.post("/", loginUser);
 router.post("/create", createUser);
+router.get("/getUser", authUser, getUser);
 
 
 module.exports = router
